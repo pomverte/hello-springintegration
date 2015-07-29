c72fac3cf042e19d790b782aa20ce276232d4c81
hvle <ha.hong.viet.le@cgi.com>
2015-07-30 01:15:09 +0200

    Hello world 101 Message -> Channel -> QueueChannel

---
+++ src/main/java/fr/cgi/tps/atp/hello/HelloOneOhOneApplication.java
@@ -0,0 +1,50 @@
+package fr.cgi.tps.atp.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.channel.QueueChannel;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.dsl.channel.MessageChannels;
+import org.springframework.messaging.support.GenericMessage;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Message -> Channel -> QueueChannel
+ * 
+ * @author hvle
+ *
+ */
+@Slf4j
+@SpringBootApplication
+@IntegrationComponentScan
+public class HelloOneOhOneApplication {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(HelloOneOhOneApplication.class, args);
+
+        DirectChannel inputChannel = context.getBean("inputChannel", DirectChannel.class);
+        inputChannel.send(new GenericMessage<String>("World"));
+
+        QueueChannel outputChannel = context.getBean("outputChannel", QueueChannel.class);
+        log.info("==> {}", outputChannel.receive().getPayload());
+    }
+
+    @Bean
+    public IntegrationFlow orders() {
+
+        return
+        // direct channel named inputChannel
+        IntegrationFlows.from("inputChannel")
+
+        // ouput queue channel attached to our service activator
+                .channel(MessageChannels.queue("outputChannel", 10))
+
+        .get();
+    }
+}
