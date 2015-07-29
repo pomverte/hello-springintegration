dcbbbc84c0e826fa83b7cb5291a5fb9e9425bec9
hvle <ha.hong.viet.le@cgi.com>
2015-07-30 01:30:06 +0200

    remove unnecessary @IntegrationComponentScan

--- src/main/java/fr/cgi/tps/atp/hello/HelloWorldApplication.java
+++ src/main/java/fr/cgi/tps/atp/hello/HelloWorldApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.integration.annotation.IntegrationComponentScan;
 import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.channel.QueueChannel;
 import org.springframework.integration.dsl.IntegrationFlow;
@@ -25,7 +24,6 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Slf4j
 @SpringBootApplication
-@IntegrationComponentScan
 public class HelloWorldApplication {
 
     public static void main(String[] args) {
